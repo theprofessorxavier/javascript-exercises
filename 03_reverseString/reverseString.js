aee1a217d6904410576d09f15794881e886c9fa4
Xavier <142161233+theprofessorxavier@users.noreply.github.com>
2025-02-28 22:04:11 +1000

    Add Functionality to 'reverseString'

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringToArray = string.split("");
+    let revString = "";
+    for(let i=(string.length-1);i>=0;i--){
+        revString+=stringToArray[i];
+    }
+    return revString;
 };
 
 // Do not edit below this line
