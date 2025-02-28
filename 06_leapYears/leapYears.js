55acd8560ad4e39659274ef97925588f14d3fee4
Xavier <142161233+theprofessorxavier@users.noreply.github.com>
2025-03-01 04:33:37 +1000

    Add Functionality To 'leapYears'

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,5 @@
-const leapYears = function() {
-
-};
+const leapYears = year => (year%4==0 && year%100!=0)?
+true:(year%400==0)?true:false;
 
 // Do not edit below this line
 module.exports = leapYears;
