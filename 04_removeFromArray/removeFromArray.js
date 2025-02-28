8141ad8a87829c98336934ec00b6dd62cdf247fc
Xavier <142161233+theprofessorxavier@users.noreply.github.com>
2025-02-28 23:49:15 +1000

    Add Functionality to 'removeFromArray'

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let elementRemoved = array;
+    for(let arg of args){
+        for(let i=array.indexOf(arg);i<array.length;){
+            if(array[i] === arg){
+                elementRemoved.splice(i, 1);
+                i=0;
+            }else{
+                i++;
+            }
+        }
+    }
+    return elementRemoved;
 };
 
 // Do not edit below this line
