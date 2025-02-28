af6d703f8b98eba699241fc9748b3570b1593a40
Xavier <142161233+theprofessorxavier@users.noreply.github.com>
2025-03-01 05:12:27 +1000

    Add Functionality to 'convertToCelsius' and 'convertToFahrenheit'

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const fiveDividedByNine = 5/9;
+  let result = (fahrenheit-32) * fiveDividedByNine;
+  return (isNotDecimal(result))?
+          result:
+          parseFloat(result.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const nineDividedByFive = 9/5;
+  let result = celsius * nineDividedByFive + 32;
+  return (isNotDecimal(result))?
+          result:
+          parseFloat(result.toFixed(1));
 };
 
+function isNotDecimal(num){
+  let check = Math.ceil(num*10)%10;
+  return (check===0)?true:false;
+}
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
