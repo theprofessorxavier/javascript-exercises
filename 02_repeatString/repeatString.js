c6fca834dd5b9e4aa59b1f240f2e8414076d74e3
Xavier <142161233+theprofessorxavier@users.noreply.github.com>
2025-02-28 21:49:58 +1000

    Add Functionality to 'repeatString'

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, numOfRepeats) {
+    let result = "";
+    for(let i = numOfRepeats; i > 0; i--){
+        result+=string;
+    }
+    result = numOfRepeats<0?"ERROR":result;
+    return result;
 };
 
 // Do not edit below this line
