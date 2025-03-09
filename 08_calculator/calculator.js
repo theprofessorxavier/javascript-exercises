b679972d8de183bee57138dd86f364546ba44b73
Xavier <142161233+theprofessorxavier@users.noreply.github.com>
2025-03-09 18:27:57 +1000

    Solve 08_Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arrayOfNumbers){
+  return arrayOfNumbers.reduce((total, num)=>total+num,0);
 };
 
-const multiply = function() {
-
+const multiply = function(arrayOfNumbers) {
+  return arrayOfNumbers.reduce((total, num)=>total*num,1);
 };
 
-const power = function() {
-	
-};
+const power = function(num1, num2) {
+	return num1**num2;
+}; 
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	for(let i = num-1; i > 0; i--){
+    num*=i;
+  }
+  if(num===0){
+    num=1;
+  }
+  return num;
 };
 
 // Do not edit below this line
