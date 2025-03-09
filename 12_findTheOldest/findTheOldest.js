19f79b4fdeaa37df73959b13e29e189feab29227
Xavier <142161233+theprofessorxavier@users.noreply.github.com>
2025-03-09 21:41:35 +1000

    Solve 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest = people.sort((person1, person2)=>{
+        let val=0;
+        if(person1.yearOfDeath === undefined || person2.yearOfDeath === undefined){
+            val = (person1.yearOfBirth<person2.yearOfBirth)?-1:1;
+        }else{
+            val = ((person1.yearOfDeath-person1.yearOfBirth)>(person2.yearOfDeath-person2.yearOfBirth))?-1:1;
+        }
+        return val;
+    });
+    return oldest[0];
 };
 
 // Do not edit below this line
