b4f05c898dcc767fbf9c53aacf23a25bdc003497
Xavier <142161233+theprofessorxavier@users.noreply.github.com>
2025-03-09 21:04:34 +1000

    Solve 10_fibonnaci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(position) {
+    position = parseInt(position);
+    let n1 = 0, n2 = 1, temp = 0;
+    let answer = (position>=0)?0:"OOPS";
+    for(let i=position-1; i>=0; i--){
+        answer = temp = n2;
+        n2 += n1;
+        n1 = temp;
+    }
+    return answer;
 };
 
 // Do not edit below this line
