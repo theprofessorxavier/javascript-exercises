b865a5c4694df4aa6e28d8bf21eaa2fa3db9eda4
Xavier <142161233+theprofessorxavier@users.noreply.github.com>
2025-03-09 19:36:12 +1000

    Solve 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let original = reversed = string
+    .toLowerCase()
+    .split('')
+    .filter((letter)=>letter!=' '&&
+                      letter!=','&&
+                      letter!='!'&&
+                      letter!='?'&&
+                      letter!='.');
+    original = original.join('');
+    reversed = reversed.reverse().join('');
+    return original === reversed;
 };
 
 // Do not edit below this line
